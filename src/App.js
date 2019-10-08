72c81e5e64ef21834d8f139f412ef452bd84909f
german juarez morales <ninganegro@hotmail.com>
2019-10-08 10:22:24 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Encabezado from './components/Encabezado';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Encabezado tema={"Tecnología"} />
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
